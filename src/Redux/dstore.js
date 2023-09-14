fe602cdcb0ef90a7ad7c086eac197563cfde1b88
Charly <icharlieanne@gmail.com>
2023-09-14 12:19:13 +0100

    added img

--- src/Redux/dstore.js
+++
@@ -1,9 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit';
-import moviesReducer from './movieSlice';
-const store = configureStore({
-  reducer: {
-    movies: moviesReducer,
-  },
-});
-
-export default store;
